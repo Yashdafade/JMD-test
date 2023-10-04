c88cb6c8d2413f1cd60ff3bb722245fd6648ef17
Yash Dafade <119852684+Yashdafade@users.noreply.github.com>
2023-10-04 13:30:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// let menuicon = document.getElementById("menu-icon")
+// let nav = document.getElementsByClassName('navbar')
+
+// menuicon.addEventListener("click", ()=>{
+//     menuicon.classList.toggle('bx-x')
+//     nav.style.display = "block";
+    
+// })
+// menuicon.onclick = () =>{
+//     menuicon.classList.toggle('bx-x')
+//     nav.style.display
+// }
+
+let menuicon = document.getElementById("menu-icon");
+let nav = document.querySelector('.navbar');
+
+
+menuicon.addEventListener("click", () => {
+    menuicon.classList.toggle('bx-x');
+    if (nav.style.transform === "translateX(0%)") {
+        nav.style.transform = "translateX(100%)";
+    } else {
+        nav.style.transform = "translateX(0%)";
+    }
+});
