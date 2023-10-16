e71246d77d8ecf27116a9c6225f607d840a5b2ac
Yash Dafade <119852684+Yashdafade@users.noreply.github.com>
2023-10-16 18:35:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ menuicon.addEventListener("click", () => {
     }
 });
 
+
+const elementsToAnimate = document.querySelectorAll('.scroll-fade');
+
+const isInViewport = (element) => {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+const animateOnScroll = () => {
+    elementsToAnimate.forEach((element, index) => {
+        if (isInViewport(element) && !element.classList.contains('animated')) {
+            setTimeout(() => {
+                element.classList.add('animated');
+            }, index * 100); // Adjust the delay (100ms) between each element as needed.
+        }
+    });
+};
+
+window.addEventListener('scroll', animateOnScroll);
+window.addEventListener('load', animateOnScroll);
+
+
+const footer = document.getElementById('myFooter');
+
+
+
+
+
